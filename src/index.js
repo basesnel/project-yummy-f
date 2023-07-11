95110b0ca946edb5d96d3cd75c20b3385641feec
Oleksii Bolotin <samkiral73@gmail.com>
2023-07-11 18:16:03 +0200

    add: list tabs,parsing from temp array

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from '@mui/material';
 import theme from 'theme';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistedStore } from 'redux/store';
@@ -11,15 +11,15 @@ import { Provider } from 'react-redux';
 import Loader from 'components/Loader/Loader';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<Loader />} persistor={persistedStore}>
-        <BrowserRouter basename="/project-yummy-f">
-          <ThemeProvider theme={theme}>
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate loading={<Loader />} persistor={persistedStore}>
+                <BrowserRouter basename="/project-yummy-f">
+                    <ThemeProvider theme={theme}>
+                        <App />
+                    </ThemeProvider>
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,13 +1,58 @@
+import { Box, Tab, Tabs, ThemeProvider } from '@mui/material';
+import { useState, useEffect } from 'react';
+// import plug from 'assets/images/PreviewCategories/1.jpg';
+// import { resize } from 'helpers/resize';
+import { Container } from 'components/Container/Container';
+import theme from 'theme';
+import { useNavigate, useParams } from 'react-router-dom';
+import categoriesArr from 'tempFiles/categoriesArr';
+
 const CategoriesPage = () => {
+    // const [categorie, setCategorie] = useState('');
+    const [categories, setCategories] = useState(categoriesArr);
+    const navigate = useNavigate();
+    const { categoryName } = useParams();
+
+    useEffect(() => {
+        return () => {
+            if (categoryName === ':categoryName') {
+                setValue(categoriesArr[1]);
+                navigate(`/categories/${categoriesArr[1]}`);
+            }
+        };
+    }, [categoryName, navigate]);
+
+    // useEffect(() => {
+    //   navigate(`/categories/${categoriesArr[1]}`);
+
+    // }, [newValue])
+
+    const [value, setValue] = useState(categoryName);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        navigate(`/categories/${newValue}`);
+    };
     return (
-        <h1
-            style={{
-                padding: '200px',
-                textAlign: 'center',
-            }}
-        >
-            CategoriesPage
-        </h1>
+        <section>
+            <h2>Categories</h2>
+            <Box sx={{ width: '100%' }}>
+                <Tabs
+                    sx={{}}
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="wrapped label tabs example"
+                >
+                    {categoriesArr.map(({ _id, name }) => (
+                        <Tab
+                            key={_id.$oid}
+                            value={name.toLowerCase()}
+                            label={name}
+                        />
+                    ))}
+                </Tabs>
+            </Box>
+            <h2>{categoryName}</h2>
+        </section>
     );
 };
 
