a7d5553b08d8873302fbcea5439e7e14b3322297
Maksym-Palii <paliym_@ukr.net>
2023-07-11 20:20:22 +0300

    styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,24 @@
 import {
-  NavContainer,
-  StyledLink,
-  WrapperSearchIcon,
+    NavContainer,
+    StyledLink,
+    TextContainer,
+    WrapperSearchIcon,
 } from './Navigation.styled';
 
 const Navigation = () => {
-  return (
-    <NavContainer>
-      <StyledLink to="/categories/:categoryName">Categories</StyledLink>
-      <StyledLink to="/add">Add recipes</StyledLink>
-      <StyledLink to="/my">My recipes</StyledLink>
-      <StyledLink to="/favorite">Favorites</StyledLink>
-      <StyledLink to="/shopping-list">Shopping list</StyledLink>
-      <StyledLink to="/search">
-        <WrapperSearchIcon />
-      </StyledLink>
-    </NavContainer>
-  );
+    return (
+        <NavContainer>
+            <StyledLink to="/categories/:categoryName">Categories</StyledLink>
+            <StyledLink to="/add">Add recipes</StyledLink>
+            <StyledLink to="/my">My recipes</StyledLink>
+            <StyledLink to="/favorite">Favorites</StyledLink>
+            <StyledLink to="/shopping-list">Shopping list</StyledLink>
+            <StyledLink to="/search">
+                <WrapperSearchIcon />
+                <TextContainer>Search</TextContainer>
+            </StyledLink>
+        </NavContainer>
+    );
 };
 
 export default Navigation;
--- src/components/Navigation/Navigation.styled.js
+++ src/components/Navigation/Navigation.styled.js
@@ -1,4 +1,4 @@
-import { SIZE } from 'constants';
+import { SIZE, COLOR } from 'constants';
 import { NavLink } from 'react-router-dom';
 import { styled } from 'styled-components';
 import { ReactComponent as SearchHeaderIcon } from 'assets/images/header/searchHeader.svg';
@@ -8,56 +8,56 @@ export const NavContainer = styled.nav`
     flex-direction: column;
     gap: 32px;
 
-    @media screen and (min-width: ${SIZE.mobile}) {
+    @media screen and (min-width: ${SIZE.tablet}) {
         gap: 40px;
     }
 
-    @media screen and (min-width: ${SIZE.tablet}) {
+    @media screen and (min-width: ${SIZE.desktop}) {
         flex-direction: row;
         gap: 30px;
-    }
-
-    @media screen and (min-width: ${SIZE.desktop}) {
-        display: block;
         margin-left: 187px;
-        padding-top: 10px;
-        padding-bottom: 11px;
+        align-items: center;
     }
 `;
 
 export const StyledLink = styled(NavLink)`
-    color: #23262a;
+    display: flex;
+    gap: 8px;
+    justify-content: center;
+    color: ${COLOR.dark};
     font-family: Poppins;
     font-size: 18px;
     font-weight: 500;
     line-height: 1;
     text-decoration: none;
-
-    @media screen and (min-width: ${SIZE.mobile}) {
-        font-size: 24px;
-    }
-
+    letter-spacing: -0.36px;
     @media screen and (min-width: ${SIZE.tablet}) {
-        font-size: 14px;
-        line-height: 1.6;
-    }
-
-    &.active {
-        color: #8baa36;
-    }
-    &:hover,
-    &:focus {
-        color: #8baa36;
+        font-size: 24px;
+        letter-spacing: -0.48px;
     }
 
     @media screen and (min-width: ${SIZE.desktop}) {
-        margin-right: 30px;
+        font-size: 14px;
+        line-height: 1.6;
+        letter-spacing: 0;
+        padding-top: 10px;
+        padding-bottom: 11px;
+    }
+
+    &.active {
+        color: ${COLOR.hover};
+    }
+    &:hover,
+    &:focus {
+        color: ${COLOR.hover};
     }
 `;
 
 export const WrapperSearchIcon = styled(SearchHeaderIcon)`
-    // display: inline-flex;
-    // align-items: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    // fill: inherit;
 
     @media screen and (min-width: ${SIZE.mobile}) {
         width: 20px;
@@ -68,4 +68,21 @@ export const WrapperSearchIcon = styled(SearchHeaderIcon)`
         width: 24px;
         height: 24px;
     }
+`;
+
+export const TextContainer = styled.p`
+    color: inherit;
+    font-family: Poppins;
+    font-size: 18px;
+    font-weight: 500;
+    line-height: 1;
+    text-decoration: none;
+    letter-spacing: -0.36px;
+    @media screen and (min-width: ${SIZE.tablet}) {
+        font-size: 24px;
+        letter-spacing: -0.48px;
+    }
+
+    @media screen and (min-width: ${SIZE.desktop}) {
+        display: none;
 `;
