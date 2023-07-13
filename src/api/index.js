38e6a39d7efbae684829bbcafc57466a20f272a7
Vladyslav Kupchuk <kupchuk.vlad@gmail.com>
2023-07-13 23:09:22 +0300

    FIXED: size of recipes img in MainPage

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import fetchCategories from './categories';
 import fetchRecipies from './recipies';
+import fetchRecipesMainPage from './mainPage'
 
-const API = { fetchCategories, fetchRecipies };
+const API = { fetchCategories, fetchRecipies, fetchRecipesMainPage };
 
 export default API;
