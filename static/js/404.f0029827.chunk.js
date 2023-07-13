f960630fb92770be0d0b31a87218d2dc47f40bc3
basesnel <111281846+basesnel@users.noreply.github.com>
2023-07-13 21:48:23 +0000

    Deploying to gh-pages from @ basesnel/project-yummy-f@29201fc77fb8fdda1396d7a800cbd872645963e5 🚀

--- static/js/404.f0029827.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[404],{6139:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184),c=function(){return(0,r.jsx)("h1",{style:{padding:"200px",textAlign:"center"},children:"MyRecipesPage"})}}}]);
-//# sourceMappingURL=404.f0029827.chunk.js.map
