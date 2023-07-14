b933679f76e67c6e208e3606fefac7f7a728432e
basesnel <basesnel@gmail.com>
2023-07-14 12:41:01 +0300

    fixed recepies + users url axios

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://project-yummy-b.onrender.com';
+import { baseUrl } from 'constants';
+
+axios.defaults.baseURL = baseUrl;
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
