7c7b5873eb56aca24a048cc6cc6da4acd4e907a8
basesnel <basesnel@gmail.com>
2023-07-15 15:50:58 +0300

    verification page prepared

--- src/components/Verification/Verification.jsx
+++ src/components/Verification/Verification.jsx
@@ -1,3 +1,4 @@
+import VerifyFeedback from 'components/Verification/VerifyFeedback/VerifyFeedback';
 import SigninImg from 'components/Verification/VerificationImg';
 import { SigninContainer } from 'components/Verification/Verification.styled';
 
@@ -5,6 +6,7 @@ export default function Verification() {
   return (
     <SigninContainer>
       <SigninImg />
+      <VerifyFeedback />
     </SigninContainer>
   );
 }
