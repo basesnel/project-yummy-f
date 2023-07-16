832c7f7b05410cdd8440e65d677972740e311174
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-07-16 14:17:42 +0300

    edit api

--- src/api/search.js
+++ src/api/search.js
@@ -4,9 +4,7 @@ import { baseUrl } from 'constants';
 axios.defaults.baseURL = baseUrl;
 
 const fetchSearchResults = async ({ directory, selector, query }) => {
-  const response = await axios.get(
-    `api/${directory}/search?${selector}=${query}`
-  );
+  const response = await axios.get(`${directory}/search?${selector}=${query}`);
   return response.data;
 };
 
