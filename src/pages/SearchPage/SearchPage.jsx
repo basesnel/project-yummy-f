1bac2fa288b6f22cffcfc8415c962b1c6b074bf2
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-07-16 16:37:00 +0300

    add search by title and ingredient

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,19 +1,9 @@
-import ThemeWrap from 'components/SharedLayout/SharedLayoutStyled';
+// import ThemeWrap from 'components/SharedLayout/SharedLayoutStyled';
 
 import { SearchPageComponent } from 'components/SearchPage/SearchPageComponent';
-import ContainerSection from 'components/ContainerSection/ContainerSection';
-import { Container } from 'components/Container/Container';
 
 const SearchPage = () => {
-  return (
-    <ThemeWrap>
-      <ContainerSection>
-        <Container>
-          <SearchPageComponent />
-        </Container>
-      </ContainerSection>
-    </ThemeWrap>
-  );
+  return <SearchPageComponent />;
 };
 
 export default SearchPage;
