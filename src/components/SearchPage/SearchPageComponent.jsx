264280bed47ad9bbbdf25ec72e251ce83bbe4eb4
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-07-17 13:27:14 +0300

    add search from Main, query validation

--- src/components/SearchPage/SearchPageComponent.jsx
+++ src/components/SearchPage/SearchPageComponent.jsx
@@ -10,32 +10,48 @@ import { SearchInput } from './SearchInput/SearchInput';
 import { SearchTypeSelector } from './SearchTypeSelector/SearchTypeSelector';
 import ContainerSection from 'components/ContainerSection/ContainerSection';
 import { Container } from 'components/Container/Container';
+import Loader from 'components/Loader/Loader';
 
 export const SearchPageComponent = () => {
   const [selector, setSelector] = useState('title');
   const [recipieArr, setRecipieArr] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const getCards = async query => {
-    // console.log(query, selector);
     const queryData = { directory: 'recipes', selector, query };
     if (selector === 'ingredients') {
       queryData.directory = 'ingredients';
       queryData.selector = 'ingredient';
     }
-    const res = await API.fetchSearchResults(queryData);
-    setRecipieArr(res);
-    // console.log(res);
+    setIsLoading(true);
+    try {
+      const res = await API.fetchSearchResults(queryData);
+
+      setRecipieArr(res);
+    } catch (error) {
+      setRecipieArr(null);
+    }
+    setIsLoading(false);
   };
+
   return (
     <Container>
       <ContainerSection>
         <TitleContainer>
           <MainTitle title={'Search'} />
         </TitleContainer>
-        <SearchInput ver="mobile" getCards={getCards} />
-        <SearchInput ver="tablet" getCards={getCards} />
+        <SearchInput
+          ver="mobile"
+          getCards={getCards}
+          setRecipieArr={setRecipieArr}
+        />
+        <SearchInput
+          ver="tablet"
+          getCards={getCards}
+          setRecipieArr={setRecipieArr}
+        />
 
         <SearchTypeSelector setSelector={setSelector} />
-
+        {isLoading && <Loader />}
         {recipieArr ? (
           <SearchedRecipesList recipieArr={recipieArr} />
         ) : (
