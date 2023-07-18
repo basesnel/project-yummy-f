80ad4e7f7775f8dc6ae06be9c759eb3ad9ef6f4a
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-07-18 17:46:46 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -4,6 +4,8 @@ import { Outlet } from 'react-router-dom';
 import Loader from 'components/Loader/Loader';
 import { Footer } from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
+
+import MotivatingModal from 'components/MotivatingModal/MotivatingModal';
 import { useAuth } from 'hooks';
 
 // import THEME from 'theme';
@@ -17,6 +19,7 @@ const SharedLayout = ({ onTheme }) => {
       <Suspense fallback={<Loader />}>
         <main>
           <Outlet />
+          <MotivatingModal />
         </main>
       </Suspense>
       {isLoggedIn ? <Footer /> : null}
