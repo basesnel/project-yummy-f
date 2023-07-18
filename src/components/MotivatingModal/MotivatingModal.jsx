da1f29eb26823e8671cbcc5255575fc0e56bb755
Bohdan-Dachkovsky <bohdandachkovsky@gmail.com>
2023-07-18 15:18:04 +0300

    Update MotivatingModal.jsx

--- src/components/MotivatingModal/MotivatingModal.jsx
+++ src/components/MotivatingModal/MotivatingModal.jsx
@@ -29,7 +29,7 @@ const MotivatingModal = () => {
   //            </script>
 
   return (
-    <Area onClick={handleShow}>
+    <Area id="content-modal" onClick={handleShow}>
       <Modal
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
