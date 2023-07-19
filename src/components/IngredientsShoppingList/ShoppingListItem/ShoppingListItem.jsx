d4700565a06aa68aa7ee75b6a8396d53f436b853
Oleksii Bolotin <samkiral73@gmail.com>
2023-07-19 21:55:39 +0200

    fix: styled for theming

--- src/components/IngredientsShoppingList/ShoppingListItem/ShoppingListItem.jsx
+++ src/components/IngredientsShoppingList/ShoppingListItem/ShoppingListItem.jsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@mui/material';
 import {
   BtnRemove,
   IconRemove,
@@ -10,16 +11,25 @@ import {
 } from './ShoppingListItem.styled';
 
 const ShoppingListItem = () => {
+  const theme = useTheme();
   return (
     <Item>
       <WrapperPhoto>
         <Photo src="" alt="product" />
       </WrapperPhoto>
-      <Text>Product</Text>
+      <Text style={{ color: theme.palette.text.primary }}>Product</Text>
       <LeftContainer>
         <Number>5</Number>
-        <BtnRemove>
-          <IconRemove />
+        <BtnRemove
+          style={{
+            color: theme.palette.text.primary,
+            backgroundColor: 'transparent',
+          }}
+        >
+          <IconRemove viewBox="0 0 20 20" fill="none">
+            <path d="M15.625 4.375L4.375 15.625" stroke="currentColor" />
+            <path d="M15.625 15.625L4.375 4.375" stroke="currentColor" />
+          </IconRemove>
         </BtnRemove>
       </LeftContainer>
     </Item>
