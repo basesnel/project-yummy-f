6c02f060434438a4e994ea57bb44bfaa0d2ec853
Maksym-Palii <paliym_@ukr.net>
2023-07-19 22:01:53 +0300

    Shopping List Page Styles

--- src/components/IngredientsShoppingList/ShoppingList/ShoppingList.styled.js
+++ src/components/IngredientsShoppingList/ShoppingList/ShoppingList.styled.js
@@ -1,22 +1,15 @@
 import { styled } from 'styled-components';
-import { SIZE} from 'constants';
+import { SIZE } from 'constants';
 
 export const WrapperList = styled.ul`
-  margin: 24px 0 0 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  gap: 16px;
-  padding-left: 20px;
-
-  @media screen and (min-width: ${SIZE.mobile}) {
-    margin-top: 32px;
-    gap: 24px;
-    
-  }
+  margin-top: 24px;
 
   @media screen and (min-width: ${SIZE.tablet}) {
+    margin-top: 32px;
+  }
+
+  @media screen and (min-width: ${SIZE.desktop}) {
     margin-top: 50px;
-    
+    padding: 0 40px;
   }
 `;
--- src/components/IngredientsShoppingList/ShoppingListItem/ShoppingListItem.styled.js
+++ src/components/IngredientsShoppingList/ShoppingListItem/ShoppingListItem.styled.js
@@ -1,125 +1,155 @@
-import { SIZE, COLOR } from 'constants';
+import { TRANSITION } from 'constants';
+import { SIZE } from 'constants';
 import { styled } from 'styled-components';
-
-
-
-
+import { ReactComponent as IconRemoveX } from 'assets/images/modal/x.svg';
 
 export const Item = styled.li`
- 
-  padding: 0 0px;
-  border-radius: 8px;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  align-items: flex-start;
+  border-bottom: 1px solid #e0e0e0;
+
+  padding-bottom: 24px;
+  margin-bottom: 24px;
+  max-width: 343px;
 
   @media screen and (min-width: ${SIZE.tablet}) {
-    padding: 0 20px;
+    padding-bottom: 42px;
+    margin-bottom: 45px;
+    max-width: 704px;
   }
 
   @media screen and (min-width: ${SIZE.desktop}) {
-    padding: 0 0px;
+    padding-bottom: 43px;
+    max-width: 1160px;
   }
 `;
 
 export const WrapperPhoto = styled.div`
-  margin: 0;
-  display: flex;
-  gap: 8px;
-  justify-content: space-between;
-  align-items: center;
+  overflow: hidden;
+  width: 60px;
+  height: 60px;
+  border-radius: 6px;
+  background-color: #ebf3d4;
+  padding: 6px;
 
-  img {
-  
-    background-color: #ebf3d4;
+  border: 1px solid black;
+
+  @media screen and (min-width: ${SIZE.tablet}) {
     width: 93px;
     height: 97px;
-    border: 1px solid black;
     border-radius: 8px;
+  }
+
+  @media screen and (min-width: ${SIZE.desktop}) {
+    padding: 8px 6px;
+  }
+`;
+
+export const Photo = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Text = styled.p`
+  color: #3e4462;
+  font-family: Poppins;
+  font-weight: 500;
+  font-size: 10px;
+  line-height: 1.2;
+
+  margin-left: 10px;
+
+  @media screen and (min-width: ${SIZE.tablet}) {
+    font-size: 16px;
+    line-height: 1.5;
+
+    margin-left: 16px;
+  }
+`;
+
+export const LeftContainer = styled.div`
+  width: 116px;
+  margin-left: auto;
+  display: flex;
+  @media screen and (min-width: ${SIZE.tablet}) {
+    width: 243px;
+  }
+  @media screen and (min-width: ${SIZE.desktop}) {
+    width: 290px;
+  }
+`;
+
+export const Number = styled.p`
+  color: #fafafa;
+  font-family: Poppins;
+  font-size: 10px;
+  font-weight: 600;
+  line-height: normal;
+  border-radius: 4px;
+  background: #8baa36;
+  padding: 4px 14px;
+  margin-right: 46px;
+
+  @media screen and (min-width: ${SIZE.tablet}) {
+    font-size: 18px;
+    padding: 4px 28px;
+    margin-right: 110px;
+  }
+
+  @media screen and (min-width: ${SIZE.desktop}) {
+    margin-right: 174px;
+  }
+`;
+
+export const BtnRemove = styled.button`
+  text-decoration: none;
+  transition: ${TRANSITION.forHoverBgColor}, ${TRANSITION.forHoverColor};
+background-color:#fafafa;
+
+
+  border: none;
+  width: 20px;
+  height: 20px;
+  // margin-right:19px
+  // margin-left:46px;
+
+
+  @media screen and (min-width: ${SIZE.tablet}) {
+   
+    width: 38px;
+    height: 38px;
   
   }
 
-  p {
-    color: inherit;
-    font-family: Poppins;
-    font-size: 12px;
-    font-weight: 500;
-    line-height: 1.2;
-    
-  }
-  @media screen and (min-width: ${SIZE.mobile}) {
-    gap: 10px;
-
-    img {
-      padding: 17px 16px;
-      width: 60px;
-      height: 60px;
-    }
-
-    p {
-      font-size: 12px;
-      line-height: 1;
-    }
-  }
-
-  @media screen and (min-width: ${SIZE.tablet}) {
-    gap: 16px;
-
-    img {
-      padding: 26px;
-      width: 93px;
-      height: 97px;
-    }
-    p {
-      font-size: 24px;
-      line-height: 1;
-    }
-    
-  }
-
   @media screen and (min-width: ${SIZE.desktop}) {
-    gap: 40px;
+   
+    width: 44px;
+    height: 44px;
+   
+  }
+
+  &:hover,
+  &:focus {
+    
+    }
+
+   
   }
 `;
 
-export const MeasureAndCheckbox = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  gap: 28px;
-
-  div:first-child {
-    background-color: ${COLOR.hover};
-    border-radius: 4px;
-    padding: 4px;
-
-    p {
-      color: ${COLOR.second};
-      text-align: right;
-      font-family: Poppins;
-      font-size: 10px;
-      font-weight: 600;
-    }
-  }
-
-  @media screen and (min-width: ${SIZE.mobile}) {
-    //  gap: 78px;
-
-    div:first-child {
-      padding: 4px 8px;
-
-      p {
-        font-size: 18px;
-      }
-    }
-  }
+export const IconRemove = styled(IconRemoveX)`
+  width: 14px;
+  height: 14px;
+  stroke: currentColor;
 
   @media screen and (min-width: ${SIZE.tablet}) {
-    gap: 78px;
+    width: 22px;
+    height: 22px;
   }
 
   @media screen and (min-width: ${SIZE.desktop}) {
-    gap: 150px;
+    width: 24px;
+    height: 24px;
   }
 `;
-
